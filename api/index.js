3a6619d91d85a260c58454f8109740a0767b77dd
safinamoideen <122775542+moideensafina@users.noreply.github.com>
2025-02-05 22:22:25 +0530

    install react js and tailwind css and create first template

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
+import postRoutes from './routes/post.route.js';
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+}).then(
+  con =>{console.log(`mongo Db is connected ${con.connection.host}`)}
+).catch((err)=>{
+  console.log(err);
+})
+const app = express();
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+  });
+  
+
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+app.use('/api/post', postRoutes);
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import Dashboard from './Pages/Dashboard'
+import Projects from './Pages/Projects'
+import SignUp from './Pages/SignUp'
+import Header from './components/Header';
+import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './Pages/UpdatePost';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+    <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
+
+        </Route>
+      
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+  )
+}
